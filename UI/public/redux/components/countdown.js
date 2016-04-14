c313783b1028f796c7476ef2bfea41a3aa56530b
daniel.gomez <dani91.gomez@gmail.com>
2016-04-14 12:25:11 -0300

    Added test React component

--- UI/public/redux/components/countdown.js
+++ UI/public/redux/components/countdown.js
@@ -1,4 +1,40 @@
-ReactDOM.render (
-    <h1>Hello, world!</h1>,
-    document.getElementById('example')
-);
+
+class Countdown extends  React.Component{
+
+    constructor(props){
+        super(props);
+        this.getCountdown().then(function(json){
+            this.state = json;
+            alert(this.state);
+        })
+
+    }
+
+    componentDidMount() {
+        setInterval(this.tickCountdown, 1000);
+    }
+
+    tickCountdown() {
+        var countdown = this.getCountdown();
+        alert(countdown);
+        this.state = countdown;
+    }
+
+    getCountdown() {
+       return new Promise(function(resolve, reject) {
+           $.get("/countdown", function (result) {
+               var json = JSON.stringify(result);
+
+               resolve(json);
+           });
+
+        });
+
+    }
+    render() {
+        return <h1>Hola {this.state}</h1>;
+    }
+}
+
+
+ReactDOM.render(<Countdown />,   document.getElementById('example'));
